f435f010e2eb0cd7669d770cf5b6b96d205418dd
unknown <seimirmideros@gmail.com>
2026-06-20 20:13:44 -0500

    chore: enable CORS, serve public, add /sentry-debug and return Sentry event id on errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import './src/instrument.js';
 import Sentry, { setupExpressErrorHandler } from './src/instrument.js';
 import express from 'express';
+import cors from 'cors';
 import { config } from './config/env.js';
 import authRoutes from './routes/auth.routes.js';
 import resourceRoutes from './routes/resource.routes.js';
@@ -10,6 +11,12 @@ const app = express();
 // Sentry request/tracing handlers deben ir antes de las rutas
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
+
+// Habilitar CORS para pruebas desde el front
+app.use(cors());
+
+// Servir archivos estáticos desde /public si necesitas una página de prueba
+app.use(express.static('public'));
 
 app.use(express.json());
 
@@ -22,6 +29,11 @@ app.use('/', resourceRoutes);
 
 // Registrar el manejador de errores de Sentry justo después de las rutas
 setupExpressErrorHandler(app);
+
+app.use((err, req, res, next) => {
+  // res.sentry viene del handler de Sentry
+  res.status(500).json({ errorId: res.sentry || null, message: 'Internal server error' });
+});
 
 app.listen(config.PORT, () => {
     console.log(`Server running on http://localhost:${config.PORT}`);
